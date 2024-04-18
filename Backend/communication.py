8900834aec9254ce2092296ae1e788970875e3d0
Areyan Rastawan <areyanr@hotmail.com>
2024-04-18 00:21:53 -0700

    Database/server setup

---
+++ Backend/communication.py
@@ -0,0 +1,11 @@
+import mysql.connector
+from mysql.connector import Error
+
+db_config = {
+    'host': 'ix-dev.cs.uoregon.edu',
+    'port': 3056,
+    'user': 'group6',
+    'password': 'group6',
+    'database': 'ara_db'
+}
+#server for the database
