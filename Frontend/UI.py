e5324d72afa76ecc6cf0f9ec9b01d16e6faf1e4c
milesa04 <tarsaltag@gmail.com>
2024-04-17 18:49:09 -0700

    Username/Password Entry
    Created text boxes that take in username and password and store them as a variable, currently only prints welcome to the terminal.

--- Frontend/UI.py
+++ Frontend/UI.py
@@ -2,24 +2,50 @@ import tkinter as tk
 from tkinter import *
 
 
-class User():
-    def __init__(self, name, password):
-        self.name = name
-        self.password = password
-        self.correct_password = False
 
 
+#class User():
+#   def __init__(self, username, user_passw):
+#       self.name = username
+#       self.password = user_passw
+        
+
+
+def retrieve_input():
+    global username
+    global password
+    username = username_textbox.get()
+    password = password_textbox.get()
+    if username == "Admin" and password == "Admin":
+        print("Welcome")
 
 
 if __name__ == "__main__":
-    window = tk.Tk()
-    window.title("SQ3R Helper")
-    window.configure(background="grey")
-    #window.minsize(800,800)
-    window.geometry("800x800+100+100")
-    window.mainloop()
+    root = tk.Tk()
+    root.title("SQ3R Helper")
+    root.configure(background="grey")
+    root.minsize(600,600)
+    root.geometry("800x800+100+100")
 
 
+    username_textbox = tk.Entry(root)
+    username_textbox.pack()
+
+    password_textbox = tk.Entry(root)
+    password_textbox.pack()
+
+    
+    enter_button = Button(root, text="Enter", command=retrieve_input)
+    enter_button.pack()
+
+
+
+
+
+
+
+
+    root.mainloop()
 
 
 
