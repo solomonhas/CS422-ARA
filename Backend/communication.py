5b8b4b02497bb774f1a51c22214fc2a4a39c8279
Areyan Rastawan <areyanr@hotmail.com>
2024-04-21 18:26:59 -0700

    table plannig for data

--- Backend/communication.py
+++ Backend/communication.py
@@ -8,42 +8,16 @@ db_config = {
     'password': 'group6',
     'database': 'ara_db'
 }
+"""
+User Table holds 
+Username 
+User ID # it is auto incramented everytime a new user is created
 
-try:
-    # Connect to the MySQL server
-    connection = mysql.connector.connect(**db_config)
 
-    if connection.is_connected():
-        db_Info = connection.get_server_info()
-        print("Connected to MySQL Server version ", db_Info)
+Notes Table holds 
+User ID // has a refrence to the other table 
+Note ID
+Note text
 
-        # Prompt user for name and message
-        name = input("Enter your name: ")
-        message = input("Enter your message: ")
+"""
 
-        # Get cursor
-        cursor = connection.cursor()
-
-        # Execute query to insert data into the Users table
-        cursor.execute("INSERT INTO Users (username, message) VALUES (%s, %s)", (name, message))
-
-        # Commit the transaction
-        connection.commit()
-
-        print("Message inserted successfully!")
-
-        # Print contents of Users table
-        cursor.execute("SELECT * FROM Users")
-        rows = cursor.fetchall()
-        print("\nContents of Users table:")
-        for row in rows:
-            print(row)
-
-except Error as e:
-    print("Error while connecting to MySQL", e)
-finally:
-    # Close database connection
-    if (connection.is_connected()):
-        cursor.close()
-        connection.close()
-        print("MySQL connection is closed")
